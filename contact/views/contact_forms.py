11227537287c8fae722c74fe164924c3c5ab2b0e
vinicius <blanco.vinicius@uni9.edu.br>
2024-11-14 17:25:35 -0300

    Inserido add_error a classe ContatcForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.http import request
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import request
@@ -8,12 +9,32 @@ from contact.models import Contact
 class ContatcForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = ('first_name', 'last_name', 'phone',)
+    
+    def clean(self):
+        c_data = self.cleaned_data
+        
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem Erro',
+                code='invalid'
+            )
+        )
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem Erro2',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 def create(request:request.HttpRequest):
     
     if request.method == 'POST':
-        print("aqui")
+        
         context = {
             'form': ContatcForm(request.POST),
         }
